8e37846b41a0ed0845f0d1cb1bb0ca8b845a194f
Micah Algard <micahalgard@gmail.com>
2024-09-04 21:16:05 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# realestate-bot
+# Realestate Bot
+
+### Install requirements
+
+```bash
+# create venv 
+virtualenv venv
+
+# activate venv
+source venv/bin/activate
+
+# install requirements
+pip install -r requirements.txt
+```
+
+### Run webscraper
+```bash
+cd webscraper
+
+python scraper.py
+```
+
+### Overview
+
+The website being scraped is [beverly.patriotproperties.com](https://beverly.patriotproperties.com). We are using [selenium](https://selenium-python.readthedocs.io/getting-started.html) as our webdriver along with chrome. We are also using [overpy](https://pypi.org/project/overpy/) which is a python wrapper of the "Open Street Maps" API called "Overpass" to grab all the streets in beverly so we search for them on patriot properties website. This has got most of the streets but not all of them. The plan is to implement a backend service using [python django](https://docs.djangoproject.com/en/5.1/) so save the scrapped data to and build an API on top of.
---
+++ webscraper/streets_data/generate_streets_data.py
@@ -0,0 +1,62 @@
+import overpy
+import json
+
+def remove_duplicates(input_list):
+    # Convert the list to a set to remove duplicates, then back to a list
+    unique_list = list(set(input_list))
+    return unique_list
+
+def replace_street_suffixes(input_string):
+    replacements = {
+        "Avenue": "Ave",
+        "Street": "St",
+        "Road": "Rd",
+        "Lane": "Ln",
+        "Court": "Ct",
+        "Terrace": "Terr",
+        "Drive": "Dr",
+        "Circle": "Cir",
+        "Place": "Pl",
+        "Heights": "Hgts",
+        "Square": "Sq",
+        "Point": "Pt",
+        "Park": "Pk"
+    }
+
+    for old_suffix, new_suffix in replacements.items():
+        if input_string.endswith(old_suffix):
+            input_string = input_string[:-len(old_suffix)] + new_suffix
+    return input_string
+
+def get_beverly_streets():
+    # Create an Overpass API instance
+    api = overpy.Overpass()
+
+    # Define the query to search for streets in Beverly, Massachusetts
+    query = (
+        f'area["name"="Beverly"]["admin_level"="8"]["boundary"="administrative"]["type"="boundary"]["wikidata"="Q54138"];'
+        'way(area)["highway"="residential"];'
+        'out body;'
+    )
+
+    # Send the query to the API
+    result = api.query(query)
+
+    # Extract the street names
+    street_names = []
+    for way in result.ways:
+        street_name = way.tags.get("name")
+        if street_name:
+            new_street_name = replace_street_suffixes(street_name)
+            street_names.append(new_street_name)
+
+    return street_names
+
+if __name__ == "__main__":
+    beverly_streets = get_beverly_streets()
+    unique_streets = remove_duplicates(beverly_streets)
+    # Write the results to a JSON file
+    with open("beverly_streets.json", "w") as json_file:
+        json.dump(unique_streets, json_file, indent=4)
+
+    print("Street names have been written to 'beverly_streets.json'.")
